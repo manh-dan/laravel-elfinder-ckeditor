93e3fa8c6b020dcd935170453a95c3691484145b
manh-dan <danhuynh660@gmail.com>
2024-02-01 06:14:33 +0000

    [Fix] elFinder for CKEditor 5 in Laravel

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Function to download a file from GitHub
+download_file() {
+    local github_url="$1"
+    local target_dir="$2"
+    local file_name="$3"
+
+    # Check if cURL is installed
+    if ! command -v curl &> /dev/null; then
+        echo "cURL is not installed. Please install cURL to proceed."
+        exit 1
+    fi
+
+    # Check if the target directory exists, if not, create it
+    mkdir -p "$target_dir"
+
+    # Download the file from GitHub
+    curl -o "$target_dir/$file_name" "$github_url"
+
+    # Check whether the download process was successful
+    if [ $? -eq 0 ]; then
+        echo "Downloaded file $file_name successfully."
+    else
+        echo "An error occurred during the file download process."
+        exit 1
+    fi
+}
+
+# Path to the resources/views directory of Laravel for elFinder setup
+laravel_view_dir="resources/views/elfinder"
+download_file "https://raw.githubusercontent.com/manh-dan/laravel-elfinder-ckeditor/main/setup.blade.php" "$laravel_view_dir" "setup.blade.php"
+
+# Path to the public/packages/barryvdh/elfinder/js directory of Laravel for CKEditor 5 setup
+laravel_public_dir="public/packages/barryvdh/elfinder/js"
+download_file "https://raw.githubusercontent.com/manh-dan/laravel-elfinder-ckeditor/main/ckeditor5.js" "$laravel_public_dir" "ckeditor5.js"
---
+++ setup.blade.php
@@ -0,0 +1,11 @@
+<!-- jQuery and jQuery UI (REQUIRED) -->
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+<!-- elFinder CSS (REQUIRED) -->
+<link rel="stylesheet" type="text/css" href="{{ asset('/packages/barryvdh/elfinder/css/elfinder.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/packages/barryvdh/elfinder/css/theme.css') }}">
+<!-- elFinder JS (REQUIRED) -->
+<script src="{{ asset('/packages/barryvdh/elfinder/js/elfinder.min.js') }}"></script>
+<!-- Setup CKEditor 5 in elFinder JS (REQUIRED) -->
+<script src="{{ asset('/packages/barryvdh/elfinder/js/ckeditor5.js') }}"></script>
